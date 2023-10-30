22320b4905a0abb6da3285e743309afd17fe2c3a
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-30 22:26:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //1.iterate throgth the outer array with variale i=0 till i<array.length-1
+  for (let i = 0; i < array.length - 1; i++)
+    //2.iterate throgth the inner array with variale j=i+1 till i<array.length
+    for (let j = i + 1; j < array.length; j++) {
+      //if (array[i]+array[j]===target) return ture else return false
+      if (array[i] + array[j] === target)
+        return true
+    }
+  return false
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
-*/
+  1.iterate throgth the outer array with variale i=0 till i<array.length-1
+  2.iterate throgth the inner array with variale j=i+1 till j<array.length
+ (because we skipped the first integer in the array and started at index=1)
+  3.if (array[i]+array[j]===target) 
+    return true
+  else  
+  return false
+ */
 
 /*
   Add written explanation of your solution here
+  write a function with 2 argumants : `array`  & `target`
+  if the sum of any 2 numbers in the array equals the target  
+  return true otherwise return false 
+ 
+  examples:
+  ([1,2,3,5],5)=> true: 1,2  &  2,3  & 5,[]
+  ([3,10,11],12)=> false
+  ([5],5)=>true
 */
 
 // You can run `node index.js` to view these console logs
